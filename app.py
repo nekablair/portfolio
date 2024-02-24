7211a27ebc0e932da6fb850828ede4752c5e68ca
Neka Blair <ourblairfamily@gmail.com>
2024-02-24 16:59:40 -0600

    add gitignore file, update sql file

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template, request
-# from sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__) #<--naming the application with variable app
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://{SECRET}@localhost:5000/portfolio_site"
+                                                                #instead of local host , you would use wherever your database url and port is living online
+db = SQLAlchemy(app)#<--using db as variable to hold sqlalchemy reference to app in line 4, much quicker way to write instead of the full name
 
 posts = [
     {
@@ -18,23 +25,34 @@ posts = [
     }
 ]
 
-@app.route("/")#<--creating route with app.route decorator and endpoint forward slash
-@app.route("/home")#<--can have multiple endpoints pointing to the same page, handled by the same function
-def home():
-    return render_template("home.html", title="Home")#<--using variable name for list of dictionaries
+#creating a database model
+class MockData(db.Model): #<--making our sql data table into a class 
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(30), nullable=False) #<--something must be placed into this row
+    last_name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(35), unique=True, nullable=False)
+    ip_address = db.Column(db.String(20))
+    gender = db.Column(db.String(6))
 
-@app.route("/about")
-def about():
-    return render_template("about.html", title="About")
+    def __repr__(self):
+        return "<Name %r>" % self.name
 
-@app.route("/projects")
-def projects():#<--Kept getting an assertion error(view function mapping is overwriting an existing endpoint function: about. I debugged and found I didn't change the method name.)
-    return render_template("projects.html", title="Projects")
+    @app.route("/")#<--creating route with app.route decorator and endpoint forward slash
+    @app.route("/home")#<--can have multiple endpoints pointing to the same page, handled by the same function
+    def home():
+        return render_template("home.html", title="Home")#<--using variable name for list of dictionaries
 
-@app.route("/blog")
-def blog():
-    return render_template("blog.html", posts=posts, title="Blog")
+    @app.route("/about")
+    def about():
+        return render_template("about.html", title="About")
 
+    @app.route("/projects")
+    def projects():#<--Kept getting an assertion error(view function mapping is overwriting an existing endpoint function: about. I debugged and found I didn't change the method name.)
+        return render_template("projects.html", title="Projects")
+
+    @app.route("/blog")
+    def blog():
+        return render_template("blog.html", posts=posts, title="Blog")
 
 if __name__ == "__main__":#<--conditional using dunder, used when script runs module directly
     app.run(debug=True)#<--server in debug mode, not to be used in production
--- portfolio_site.sql
+++ portfolio_site.sql
@@ -1,27 +1,43 @@
-DROP TABLE IF EXISTS posts;
-DROP TABLE IF EXISTS projects;
-DROP TABLE IF EXISTS project_images;
+-- DROP TABLE IF EXISTS posts;
+-- DROP TABLE IF EXISTS projects;
+-- DROP TABLE IF EXISTS project_images;
+DROP TABLE IF EXISTS mock_data;
 
-CREATE TABLE posts (
-    post_id serial PRIMARY KEY,
-    title VARCHAR(100),
-    author VARCHAR(50),
-    date DATE,
-    body TEXT
+-- CREATE TABLE posts (
+--     post_id serial PRIMARY KEY,
+--     title VARCHAR(100),
+--     author VARCHAR(50),
+--     date DATE,
+--     body TEXT
+-- );
+
+-- CREATE TABLE projects (
+--     project_id serial PRIMARY KEY,
+--     tech_stack VARCHAR(255),
+--     title VARCHAR(150),
+--     link TEXT,
+--     github TEXT
+-- );
+
+-- CREATE TABLE project_images (
+--     image_id serial PRIMARY KEY,
+--     image BYTEA
+-- );
+
+
+CREATE TABLE mock_data (
+    id serial PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    email VARCHAR(35),
+    ip_address VARCHAR(20),
+    gender VARCHAR(6)
 );
 
-CREATE TABLE projects (
-    project_id serial PRIMARY KEY,
-    tech_stack VARCHAR(255),
-    title VARCHAR(150),
-    link TEXT,
-    github TEXT
-);
+COPY mock_data FROM "/home/neka/projects/portfolio-site/data/data.csv" DELIMITER "," CSV HEADER;
 
-CREATE TABLE project_images (
-    image_id serial PRIMARY KEY,
-    image BYTEA
-);
+-- \COPY mock_data FROM "/home/neka/projects/portfolio-site/data/data.csv" DELIMITER "," CSV HEADER; --in case above command does not work, use backslash and then entire command, i kept receiving permission denied errors when using w/o backslash at beginning of command
 
 -- IF NEEDING TO BRING IN CSV INTO YOUR DATABASE, USE THE FOLLOWING COMMAND
--- COPY name-of-file FROM "absolute path to file" DELIMITER ",", CSV HEADER;
+-- COPY name-of-table FROM "absolute path to file" DELIMITER ",", CSV HEADER;
+                                                                    -- ^^ this tells postgresql that the very first line is the header in csv and everything else reflects that by commas
